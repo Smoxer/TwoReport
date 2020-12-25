edca1af8b9338699459d42016abfe0faf17b6eff
Omri <omrimaor@hotmail.com>
2020-12-25 23:36:30 +0200

    Added manual cookies adding & auto reporting from yaml file

--- two_report.py
+++ two_report.py
@@ -6,6 +6,7 @@ import requests
 import sqlite3
 import warnings
 import datetime
+import yaml
 import sys
 import os
 
@@ -19,15 +20,18 @@ SQLITE_COOKIES_LOCATIONS = ['/data/data/il.idf.doch1/app_webview/Default/Cookies
 class OneReport:
     ONE_REPORT_URL = 'https://one.prat.idf.il/'
     ENSURE_LOGIN_URI = 'api/Attendance/GetReportedData'
+    REPORT_TODAY_URI = ''
     HISTORY_URI = 'api/Attendance/memberHistory'
     FINISH_URI = 'finish'
     HEADERS = {'User-Agent': FIREFOX_UA, 'Accept': 'application/json, text/plain, */*', 'Host': urlparse(ONE_REPORT_URL).netloc}
 
-    def __init__(self, debug=False):
+    def __init__(self, debug=False, cookies_file=None):
+        self.user_data = {}
+
         self._debug = debug
         self._session = requests.Session()
         self._session.headers.update(OneReport.HEADERS)
-        self._session.cookies.update(self._get_connection_cookies())
+        self._override_cookies(cookies_file)
         self._keepalive()
 
     def _keepalive(self):
@@ -38,7 +42,7 @@ class OneReport:
         self._session.get(self.ONE_REPORT_URL + self.FINISH_URI)
 
     def _log(self, message, stdout_func=print):
-        if self._debug:
+        if self._debug or stdout_func != print:
             stdout_func(message)
 
     def _decrypt_cookie(self, blob):
@@ -76,17 +80,36 @@ class OneReport:
                     result[key] = value
         return result
 
-    def ensure_login(self):
+    def _get_cookies_from_file(self, cookies_file_path):
+        if os.path.exists(cookies_file_path):
+            self._log(f'Getting cookies from {cookies_file_path}')
+            with open(cookies_file_path, 'rb') as cookies_fd:
+                return yaml.safe_load(cookies_fd)
+        self._log(f'File {cookies_file_path} was not found!', warnings.warn)
+        return {}
+
+    def _override_cookies(self, cookies_file):
+        if not cookies_file:
+            cookies = self._get_connection_cookies()
+        else:
+            cookies = self._get_cookies_from_file(cookies_file)
+        self._session.cookies.update(cookies)
+        self._log(f'Using cookies {cookies}')
+
+    def _ensure_login(self):
+        if self.user_data == {}:
+            self._log(f'You must logged in!', warnings.warn)
+
+    def login(self):
         '''
         Ensure we are logged in and get our name as a test
         '''
         ensure_login_request = self._session.get(self.ONE_REPORT_URL + self.ENSURE_LOGIN_URI)
         if ensure_login_request.status_code == 200:
-            response = ensure_login_request.json()
-            print(f"Logged in as {response['firstName']} {response['lastName']}")
-            return True
-        self._log(f"Can't login, got status code {ensure_login_request.status_code}")
-        return False
+            self.user_data = ensure_login_request.json()
+            print(f"Logged in as {self.user_data['firstName']} {self.user_data['lastName']}")
+        else:
+            self._log(f"Can't login, got status code {ensure_login_request.status_code}")
 
     def print_history(self):
         '''
@@ -97,22 +120,43 @@ class OneReport:
         for day in history['days']:
             print(f"{day['date']}\t\t{day['mainStatusDeterminedName']} - {day['secondaryStatusDeterminedName']}")
 
+    def auto_report_from_file(self, report_file_path):
+        self._ensure_login()
+        if self.user_data['cantReport']:
+            print(f"Can't report right now")
+        else:
+            if not os.path.exists(report_file_path):
+                self._log(f'File {report_file_path} does not exists!', warnings.warn)
+                sys.exit(1)
+            with open(report_file_path, 'rb') as dates_report:
+                reports = yaml.safe_load(dates_report)
+            now = datetime.datetime.now()
+            current_day = float(f'{now.day}.{now.month}')
+            if current_day not in reports:
+                self._log(f"Can't find a report for {current_day}", warnings.warn)
+            else:
+                print(f"Reporting {reports[current_day]} on today ({current_day})")
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Automatic doch1. In order for this script to work, you need to login via chrome/doch1 app (only works for rooted android phones) and choose the "Remember me" option')
     parser.add_argument('-d', '--debug', action='store_true', help='Print debug messages')
     parser.add_argument('--history', action='store_true', help='Show report history')
+    parser.add_argument('--cookies', action='store', help='Override cookies scan and provied yaml format cookies file')
+    parser.add_argument('-a', '--auto', action='store', help='Auto fill report from file')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    one_report = OneReport(args.debug)
-    if not one_report.ensure_login():
+    one_report = OneReport(args.debug, args.cookies)
+    one_report.login()
+    if one_report.user_data == {}:
         sys.exit(1)
 
     if args.history:
         one_report.print_history()
+    elif args.auto:
+        one_report.auto_report_from_file(args.auto)
     else:
         print(f'Nothing to do')
 
